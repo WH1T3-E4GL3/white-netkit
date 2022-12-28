e43ae1bc11220a84be8dd406c82b0daaad935145
white-eagle <118425907+WH1T3-E4GL3@users.noreply.github.com>
2022-12-28 19:52:43 +0530

    Update white-netkit.py

--- white-netkit.py
+++ white-netkit.py
@@ -5,7 +5,7 @@ import subprocess
 import time
 
 os.system("pip install scapy")
-os.system("pip intsall scap.all")
+os.system("pip intsall scapy.all")
 os.system("git pull")
 
 os.system("figlet White NetKit")
